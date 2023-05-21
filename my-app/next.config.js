2f9358f665cd24098a96428d3c75190c159e050e
Aashish <aashishnkumar@gmail.com>
2023-05-21 17:15:49 +0530

    Modified next.config,js, to handle unimported modules

--- my-app/next.config.js
+++ my-app/next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+// Add the code here to disable the bufferutil and utf-8-validate
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      bufferutil: false,
+      "utf-8-validate": false,
+    };
+    return config;
+  },
+};
+
+module.exports = nextConfig;
