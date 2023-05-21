a7c4b371cec9197b21e230c7c75032a80884722d
Aashish <aashishnkumar@gmail.com>
2023-05-21 17:24:14 +0530

    File name changed to fix some issues

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -4,7 +4,7 @@ import Head from "next/head";
 import Web3Modal from "web3modal";
 import { providers, Contract } from "ethers";
 import { useEffect, useRef, useState } from "react";
-import { WHITELIST_CONTRACT_ADDRESS, abi } from "./constants/page.js";
+import { WHITELIST_CONTRACT_ADDRESS, abi } from "./constants/index";
 
 export default function Home() {
   const [walletConnected, setWalletConnected] = useState(false);
